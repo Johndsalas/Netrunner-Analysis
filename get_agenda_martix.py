1be5dd93ce91e938a03c060742c582b31d80ac59
John Salas <johnsalas@MacBookPro.lan>
2026-06-25 10:36:12 -0500

    final edits

--- get_agenda_martix.py
+++ get_agenda_martix.py
@@ -1,11 +1,12 @@
-''' Script for generating a dataset for the average number of random accesses a runner needs to win a game of Netrunner 
-    based on a lorge number of simulated games
+''' 
+    Script for generating dataset for Netrunner Agendas and Accesses Study
        
     Data set includes:
-    All max and min deck sizes for the corporation player
-    All possible agenda value counts given the constraints of the current card pool (April 21, 2026)
-    All possibel agenda counts where "let Them Dream" included in the corperation deck the maximum number of times 
-    effectively reducing the number of points that the runner can steal during a game
+    All legal combinations of studied deck building features where Let Them Dream is not included in the deck
+    Additional rows simulating replacing the maximum number of 'normal' two-point agendas with Let Them Dream agendas for each of the previous builds containing two-point agendas
+    Each combination saved as one row of data
+    The access threshold was calculated and added to each row
+    Data reflects the current standard legal card pool (April 21, 2026)
 '''
 
 # imports
@@ -24,60 +25,68 @@ max_twos = 36
 max_threes = 12
 
 def main(deck_sizes, max_ones, max_twos, max_threes):
-    '''Generates a dataset showing the average number of random accesses a runner needs to win a game of Netrunner 
-       based on a lorge number of simulated games
-       
-       Data set includes:
-       All max and min deck sizes for the corporation player
-       All possible agenda value counts given the constraints of the current card pool (April 21, 2026)
-       All possibel agenda counts where "let Them Dream" included in the corperation deck the maximum number of times 
-       effectively reducing the number of points that the runner can steal during a game
-       '''
+    '''
+        Takes in 
+        List of all desired deck sizes
+        Maximum number of one point agendas (by available card pool)
+        Maximum number of three point agendas (by available card pool)
+    
+        Returns dataframe with all legal combinations of studied features 
+        Adding Additional rows simulating replacing the maximum number of 
+        'normal' two-point agendas with Let Them Dream agendas for each of 
+        the previous builds containing two-point agendas
+    '''
 
     # get empty dictionary
     agenda_dictionary = {
 
-                    "agenda_type" : [],
-                    "deck_size" : [],
-                    "agenda_points" : [],
-                    "let_them_dream_count": [],
-                    "agenda_counts" : [],
-                    "num_agendas" : [],
-                    "average_accesses" : []
+                        "agenda_type" : [],
+                        "deck_size" : [],
+                        "agenda_points" : [],
+                        "let_them_dream_count": [],
+                        "agenda_counts" : [],
+                        "num_agendas" : [],
+                        "average_accesses" : []
+                        
+                        }
 
-    }
-
-    # for each dack size
+    # for each deck size
     for deck_size in deck_sizes:
 
-        # get normal agenda point total
-        agenda_point_totals = get_agenda_point_totals(deck_size)
+            # get normal agenda point total
+            agenda_point_totals = get_agenda_point_totals(deck_size)
 
-        # for each agenda point total
-        for agenda_point_total in agenda_point_totals:
+            # for each agenda point total
+            for agenda_point_total in agenda_point_totals:
 
-            # get list of every possible agenda point combination
-            agenda_counts = get_agenda_counts(agenda_point_total, max_ones, max_twos, max_threes)
+                # get list of every possible agenda point combination
+                agenda_counts = get_agenda_counts(agenda_point_total, max_ones, max_twos, max_threes)
 
-            # agenda count and number of Let Them Dream agendas in the deck
-            ltd_agenda_counts, num_ltd = get_ltd_agenda_counts(agenda_counts)
+                # agenda count and number of Let Them Dream agendas in the deck
+                ltd_agenda_counts, num_ltd = get_ltd_agenda_counts(agenda_counts)
 
-            # add Let Them Dream agenda info to dictionary
-            agenda_type = "Let Them Dream"
+                # add Let Them Dream agenda info to dictionary
+                agenda_type = "Let Them Dream"
 
-            agenda_dictionary = fill_dictionary(agenda_dictionary, agenda_point_total, ltd_agenda_counts, num_ltd, deck_size, agenda_type)
+                agenda_dictionary = fill_dictionary(agenda_dictionary, agenda_point_total, ltd_agenda_counts, num_ltd, deck_size, agenda_type)
 
-            # add normal agenda info to dictionary
-            agenda_type = "Normal"
-            num_ltd = [0] * len(agenda_counts)
+                # add normal agenda info to dictionary
+                agenda_type = "Normal"
+                num_ltd = [0] * len(agenda_counts)
 
-            agenda_dictionary = fill_dictionary(agenda_dictionary, agenda_point_total, agenda_counts, num_ltd, deck_size, agenda_type)
-        
+                agenda_dictionary = fill_dictionary(agenda_dictionary, agenda_point_total, agenda_counts, num_ltd, deck_size, agenda_type)
+            
     return pd.DataFrame(agenda_dictionary)
 
 
 def get_agenda_point_totals(deck_size):
+    '''
+        Takes in deck size 
+        Returns list of legal agenda point 
+        totals for that deck size
+    '''
 
+    # check deck size and assign matching agenda point list
     if deck_size >= 40 and deck_size <= 44:
 
         agenda_point_totals = [18, 19]
@@ -94,9 +103,20 @@ def get_agenda_point_totals(deck_size):
 
 
 def get_agenda_counts(agenda_point_total, max_ones, max_twos, max_threes):
+    '''
+        Takes in agenda point total 
+        Maximum number of one point agendas (by available card pool)
+        Maximum number of two point agendas (by available card pool)
+        Maximum number of three point agendas (by available card pool)
+
+        Returns list of all possible combinations of three, two, and one point agendas where their total value equals the agenda point total
+    '''
+
 
     counts_list = []
 
+    # iterate through all possible combinations of one two and three point agendas
+    # using a max number for each agenda value calculated by dividing the agenda point total by the value of the agenda
     for threes in range(agenda_point_total // 3 + 1):
 
         agenda_points_after_threes = agenda_point_total - (threes*3)
@@ -107,6 +127,7 @@ def get_agenda_counts(agenda_point_total, max_ones, max_twos, max_threes):
 
             for ones in range(agenda_points_after_twos + 1):
 
+                # if the values of all of the agendas in the combination are equal to the agenda point total add that total to a list
                 if ((3*threes) + (2*twos) + ones == agenda_point_total and 
                     threes <= max_threes and
                     twos <= max_twos and
@@ -114,20 +135,32 @@ def get_agenda_counts(agenda_point_total, max_ones, max_twos, max_threes):
 
                     counts_list.append([threes, twos, ones])
 
+    # return the list of 
     return counts_list
 
 
 def get_ltd_agenda_counts(agenda_counts):
+    '''
+        Takes in list of one, two, and three point agenda combinations
+
+        Returns list of agenda counts simulating replacing the maximum number of two point agendas with Let Them Dream agendas (up to three)
+        This is simulated by raising the number of one point agendas by one and lowering the number of two point agendas by one for each replacement
+        Also returns number of replaced agendas
+    '''
 
     ltd_agenda_counts = []
     num_ltd = []
     
+    # iterate through list of agenda counts
     for agenda_count in agenda_counts:
         
+        # unpack agenda numbers
         threes = agenda_count[0]
         twos = agenda_count[1]
         ones = agenda_count[2]
 
+        # replace agenda values based on the number of two point agendas in the agenda count
+        # and value after replacement values to list of values after replacement
 
         if twos == 0:
 
@@ -157,11 +190,25 @@ def get_ltd_agenda_counts(agenda_counts):
             num_ltd.append(ltd)
 
   
-    
+    # return list of values with replacements and list of number of replacements
     return ltd_agenda_counts, num_ltd
 
 
 def fill_dictionary(agenda_dictionary, agenda_point_total, agenda_counts, ltd_num,  deck_size, agenda_type):
+    '''
+        Takes in 
+        Dictionary of agenda features
+        Agenda point total
+        List of agenda counts 
+        Number list of count of Let Them Dream agendas 
+        Deck size
+
+        calls functions to get access threshold
+
+        Fills dictionary with information for possibility matrix
+
+        Returns filled dictionary
+    '''
 
     #loop through normal agenda counts and add information to dictionary
     for agenda_count in agenda_counts:
@@ -192,48 +239,69 @@ def fill_dictionary(agenda_dictionary, agenda_point_total, agenda_counts, ltd_nu
 
 
 def get_deck_list(agenda_count, deck_size):
+    '''
+    Takes in agenda count and deck size
+    
+    Returns list of agenda values for each card in a deck constructed using the input information 
+    '''
 
     deck_list = []
 
+    # unpack number of each agenda
     threes = agenda_count[0]
 
     twos = agenda_count[1]
 
     ones = agenda_count[2]
 
+    # add the number of each agenda value in the deck to the deck list
     deck_list.extend([3] * threes)
 
     deck_list.extend([2] * twos)
 
     deck_list.extend([1] * ones)
 
+    # fill the rest of the deck list with zeros (non-agenda cards have an agenda value of 0)
     deck_list.extend([0] * (deck_size - len(deck_list)))
 
     return deck_list
 
 
 def get_accesses(deck_list):
+    '''
+    Takes in a list of agenda point values for a given deck
+    Performs digital experiment to calculate access threshold for the deck
+    Returns the access threshold for the deck
+    '''
 
     points = 0
 
-    accesses = 0
+    access_threshold = 0
 
     temp_deck = deck_list.copy()
 
+   # continue selecting cards until 7 points of cards are selected 
     while points < 7:
 
-        accesses += 1
-
+        # for each selection
+        # choose a card value at random from the deck list
         card = random.choice(temp_deck)
 
+        # add 1 to access threshold
+        access_threshold += 1
+
+        # add value of card to points
         points += card
 
+        # remove selected card from deck
         temp_deck.remove(card)
 
-    return accesses
+    return access_threshold
 
 
 if __name__ == "__main__":
 
+    # generate data from script
     df = main(deck_sizes, max_ones, max_twos, max_threes)
+    # export to excel file
     df.to_excel("accesses.xlsx", index=False)
